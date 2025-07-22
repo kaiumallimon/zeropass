ada9332bb3a04f2efdb15cd3301d59387d753bf6
Kaium Al Limon <limon.3cs@gmail.com>
2025-07-22 12:07:40 +0600

    Add Email Verifier service for email validation using ZeroBounce API
    - Implemented EmailVerifier class to verify email addresses.
    - Integrated HTTP requests to ZeroBounce API for validation.
    - Added error handling for missing API key and response status checks.

---
+++ lib/data/services/email_verifier.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+class EmailVerifier {
+  final String baseUrl = 'https://api.zerobounce.net/v2/validate';
+
+  Future<bool> verifyEmail(String email) async {
+    try {
+      final apiKey = dotenv.env['ZEROBOUNCE_API_KEY'];
+      
+      
+      print("zerobounce api: " + apiKey!);
+      
+      
+      if (apiKey == null || apiKey.isEmpty) {
+        throw Exception(
+          'ZeroBounce API key not found in environment variables.',
+        );
+      }
+
+      final requestUrl = "$baseUrl?api_key=$apiKey&email=$email";
+
+      final response = await http.get(Uri.parse(requestUrl));
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+
+        // Example: status could be 'valid', 'invalid', 'catch-all', 'unknown'
+        final status = data['status'];
+        if (status == 'valid') {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        throw Exception('Failed to verify email: ${response.statusCode}');
+      }
+    } catch (error) {
+      rethrow;
+    }
+  }
+}
